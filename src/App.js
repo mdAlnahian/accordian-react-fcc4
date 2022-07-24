bc22cda93694517218bcb9777bad2b2b18710fb2
mdAlnahian <muhammedalnahian@gmail.com>
2022-07-24 12:24:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Questions from './Questions';
 
 function App() {
+
+  const [questions ,setQuestions] = useState([]);
+
+  useEffect(()=>{
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setQuestions(data));
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3>total number of question {questions.length}</h3>
+      <div className='info'>
+        {questions.map((question) => (
+          <Questions key={question.id} question={question}></Questions>
+        ))}
+      </div>
     </div>
   );
 }
